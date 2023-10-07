a39ff3470dace4d52e1543327cd6586cf83dafd6
NIKITA <nikitay90@mail.ru>
2023-10-08 01:24:57 +0300

    project init

---
+++ src/main/java/ru/itmo/hpsproject/HpsProjectApplication.java
@@ -0,0 +1,12 @@
+package ru.itmo.hpsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpsProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/itmo/hpsproject/HpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.hpsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
