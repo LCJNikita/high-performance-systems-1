99d680439ce89475646a3d9cf77a592c31496a8a
NIKITA <nikitay90@mail.ru>
2023-11-19 23:51:21 +0300

    feat(entities): add annotations for Item and User Entities

--- src/main/java/ru/itmo/hpsproject/model/entity/ItemEntity.java
+++ src/main/java/ru/itmo/hpsproject/model/entity/ItemEntity.java
@@ -1,10 +1,19 @@
 package ru.itmo.hpsproject.model.entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity(name = "items")
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
-    private int price;
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 }
--- src/main/java/ru/itmo/hpsproject/model/entity/UserEntity.java
+++ src/main/java/ru/itmo/hpsproject/model/entity/UserEntity.java
@@ -1,11 +1,21 @@
 package ru.itmo.hpsproject.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity(name = "users")
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String login;
     private String password;
     private String email;
