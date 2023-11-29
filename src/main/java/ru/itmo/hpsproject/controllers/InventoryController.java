0ebfb9281438b613847a72e871a210f20d5de654
NIKITA <nikitay90@mail.ru>
2023-11-29 14:36:35 +0300

    feat(controller+service): add inventory logic

--- src/main/java/ru/itmo/hpsproject/controllers/InventoryController.java
+++ src/main/java/ru/itmo/hpsproject/controllers/InventoryController.java
@@ -1,23 +1,44 @@
 package ru.itmo.hpsproject.controllers;
 
+import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.itmo.hpsproject.exeptions.NotFoundException;
 import ru.itmo.hpsproject.model.dto.ItemDto;
+import ru.itmo.hpsproject.model.entity.ItemEntity;
+import ru.itmo.hpsproject.services.InventoryService;
+import ru.itmo.hpsproject.utils.DtoConverter;
 
-import java.util.Collections;
 import java.util.List;
 
 @RequestMapping(path = "/inventory")
 @RestController
 public class InventoryController {
 
-    @GetMapping
-    public ResponseEntity<List<ItemDto>> userInventory(Long userId) {
-        List<ItemDto> emptyList = Collections.emptyList();
+    private final InventoryService inventoryService;
 
-        return new ResponseEntity<>(emptyList, HttpStatus.NO_CONTENT);
+    @Autowired
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getAll(@PathVariable @Positive Long userId) {
+        try {
+            List<ItemEntity> inventoryItemsEntities = inventoryService.findUserInventory(userId);
+            List<ItemDto> inventoryItemsDtos = inventoryItemsEntities
+                    .stream()
+                    .map(DtoConverter::itemEntityToDto)
+                    .toList();
+
+            return ResponseEntity.ok(inventoryItemsDtos);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
--- src/main/java/ru/itmo/hpsproject/services/InventoryService.java
+++ src/main/java/ru/itmo/hpsproject/services/InventoryService.java
@@ -1,4 +1,33 @@
 package ru.itmo.hpsproject.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import ru.itmo.hpsproject.exeptions.NotFoundException;
+import ru.itmo.hpsproject.model.entity.ItemEntity;
+import ru.itmo.hpsproject.model.entity.UserEntity;
+import ru.itmo.hpsproject.repositories.ItemsRepository;
+import ru.itmo.hpsproject.repositories.UsersRepository;
+
+import java.util.List;
+
+@Service
 public class InventoryService {
+
+    private final ItemsRepository itemsRepository;
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public InventoryService(ItemsRepository itemsRepository, UsersRepository usersRepository) {
+        this.itemsRepository = itemsRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    public List<ItemEntity> findUserInventory(Long userId) throws NotFoundException {
+        UserEntity user = usersRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("Юзер с id: " + userId + " не найден"));
+
+        return itemsRepository.findByUser(user);
+    }
 }
