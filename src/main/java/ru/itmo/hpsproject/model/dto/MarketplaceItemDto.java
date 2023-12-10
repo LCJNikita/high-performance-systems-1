fed609ed4aca66c9ec6157de58413938de53667e
NIKITA <nikitay90@mail.ru>
2023-12-10 22:37:10 +0300

    fix(dto): adding getters/setters for dtos

--- src/main/java/ru/itmo/hpsproject/model/dto/MarketplaceItemDto.java
+++ src/main/java/ru/itmo/hpsproject/model/dto/MarketplaceItemDto.java
@@ -1,8 +1,12 @@
 package ru.itmo.hpsproject.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.itmo.hpsproject.model.enums.Rarity;
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MarketplaceItemDto {
